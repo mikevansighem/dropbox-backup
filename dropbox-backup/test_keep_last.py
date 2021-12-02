f78dad80dc1ba830e365d751c018be65424a905b
mikevansighem <36593824+mikevansighem@users.noreply.github.com>
2021-12-02 19:33:13 +0100

    Update CI

--- dropbox-backup/test_keep_last.py
+++ dropbox-backup/test_keep_last.py
@@ -2,15 +2,15 @@ from keep_last import dates_to_utc
 
 # A dummy backup list of dicts with only UTC values.
 dummy_backups_all_utc = [
-
-    {'slug': 'd6f0919b',
-    'name': 'Automated Backup 2021-12-02',
-    'date': '2021-12-02T18:24:31.573701+00:00'},
-
-    {'slug': '599e3f01',
-    'name': 'Automated Backup 2021-12-01',
-    'date': '2021-12-01T00:00:00.093490+00:00'}
-
+    {
+        'slug': 'd6f0919b',
+        'name': 'Automated Backup 2021-12-02',
+        'date': '2021-12-02T18:24:31.573701+00:00'
+        }, {
+        'slug': '599e3f01',
+        'name': 'Automated Backup 2021-12-01',
+        'date': '2021-12-01T00:00:00.093490+00:00'
+        }
     ]
 
 # Test dates_to_utc from keep_last
